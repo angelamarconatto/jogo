02ea1d08ffed150f87ae557818eb654c0ea29994
Angela <159147808+angelamarconatto@users.noreply.github.com>
2024-11-14 08:26:20 -0300

    Add files via upload

--- perguntas.js
+++ perguntas.js
@@ -1,3 +1,29 @@
-function criacartao(materia, pergunta, Resposta){
-    console.log(materia, pergunta, Resposta)
-}
+criaCartao(
+    "geopolitica",
+    "Se a clase operária tudo produz",
+    "A ela tudo pertence"
+)
+
+criaCartao(
+    "geopolitica",
+    "Quem tem medo do comunismo?",
+    "Sao os latifundistas sao os monopolistas"
+)
+criaCartao(
+    "geopolitica",
+    "Como o capitalismo afeta os trabalhadores", 
+    "O capitalismo e um grande problema pq a maioria do dinheiro vai as grandes empresario e nao a quem ele pertence que sao os trabalhadores"
+)
+criaCartao(
+    "geopolitica",
+    "Qual o símbolo do comunismo?",
+    "Foice e martelo"
+)
+criaCartao("geopolitica",
+    "Qual símbolo do capitalismo?", 
+    "O dolar"
+)
+criaCartao("geopolitica",
+    "O que o cumunismo visa?",
+    "Iqualdade"
+)
